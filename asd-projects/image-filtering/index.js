d85b32b8dd1c1eb2105f548fb4cac87d36ec5896
Kaden Kirtland I <kadenkirtland@gmail.com>
2023-06-15 18:13:25 +0000

    image filtering done, no bonus yet

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -49,38 +49,38 @@ function applyFilter(filterFunction){
 };
 
 // TODO 7: Create the applyFilterNoBackground function
-function applyFilterNoBackground(filterFunction){
+function applyFilterNoBackground(filterFunction){ // This function applies the filter without affecting the background
   var background = image[0][0];
   for (var i = 0; i < image.length; i++){
     for (var j = 0; j < image[i].length; j++){
       if (!(image[i][j] == background)){
-      var rgbString = image[i][j]; //Accessing the color of the square
-      var rgbNumbers = rgbStringToArray(rgbString);
-      filterFunction(rgbNumbers);
-      rgbString = rgbArrayToString(rgbNumbers);
-      image[i][j] = rgbString; //applying the new color to the square
+        var rgbString = image[i][j]; //Accessing the color of the square
+        var rgbNumbers = rgbStringToArray(rgbString);
+        filterFunction(rgbNumbers);
+        rgbString = rgbArrayToString(rgbNumbers);
+        image[i][j] = rgbString; //applying the new color to the square
     }
     }
   }
 }
 
 // TODO 5: Create the keepInBounds function
-function keepInBounds(num){
+function keepInBounds(num){ //This function makes sure the numbers stay in the range of rgb elements
  return Math.max(Math.min(num, 255), 0);
 }
 
 // TODO 3: Create reddify function
-function reddify (arr){
+function reddify (arr){ //This filter makes everything more red
   arr[RED] = 200;
 }
 
-// TODO 6: Create more filter functions
-function decreaseBlue(arr){
+// TODO 6: Create more filter functions 
+function decreaseBlue(arr){ // This filter makes everything less blue
   arr[BLUE] -= 50;
  arr[BLUE] = keepInBounds(arr[BLUE]);
 }
 
-function increaseGreenByBlue(arr){
+function increaseGreenByBlue(arr){ //This filter makes the pixels more green depending on how blue they are
   var x = arr[GREEN] + arr[BLUE];
   arr[GREEN] = keepInBounds(x);
 }
